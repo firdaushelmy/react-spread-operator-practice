b0259f2e3b23d3e68b6da79b85291167ee711b54
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-08-08 23:15:40 +0800

    add onChange, push state into array when onClick, and display all array item

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function App() {
+  const [todo, setTodo] = useState('');
+  const [taskLists, setTaskLists] = useState([]);
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setTodo(newValue);
+  }
+
+  function handleClick() {
+    setTaskLists(function (prevValue) {
+      return [...prevValue, todo];
+    });
+  }
+
   return (
-    <div className="container">
-      <div className="heading">
+    <div className='container'>
+      <div className='heading'>
         <h1>To-Do List</h1>
       </div>
-      <div className="form">
-        <input type="text" />
-        <button>
+      <div className='form'>
+        <input type='text' value={todo} onChange={handleChange} />
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {taskLists.map((taskList) => (
+            <li>{taskList}</li>
+          ))}
         </ul>
       </div>
     </div>
@@ -22,3 +38,9 @@ function App() {
 }
 
 export default App;
+
+//CHALLENGE: Make this app work by applying what you've learnt.
+//1. When new text is written into the input, its state should be saved.
+//2. When the add button is pressed, the current data in the input should be
+//added to an array.
+//3. The <ul> should display all the array items as <li>s
